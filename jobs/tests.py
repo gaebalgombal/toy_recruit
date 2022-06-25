07fec17ea7ee8e1997e266689ce8e82950c95b20
Kim <annewithanme@gmail.com>
2022-06-25 19:41:58 +0900

    test14

--- jobs/tests.py
+++ jobs/tests.py
@@ -3,24 +3,3 @@ from django.test import TestCase
 # Create your tests here.
 
 ## test
-test = 'This is test'
-
-test2 =  'This is test2'
-
-test3 = 'This is test3'
-
-test4 = 'This is test4'
-
-test5 = 'This is test5'
-
-test6 = 'This is test6'
-
-test7 = 'This is test7'
-
-test8 = 'This is test8'
-
-test9 = 'This is test9'
-
-test10 = 'This is test10'
-
-test11 = 'This is test11'
